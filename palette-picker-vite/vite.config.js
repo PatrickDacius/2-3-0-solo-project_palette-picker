858c8b34279bef3a2c2e779412ab343fc73fb661
Patrick Dacius <Patrickdacius27@gmail.comgit config --global credential.helper store>
2024-06-02 22:25:17 -0400

    palette picker fin attempt two

--- palette-picker-vite/vite.config.js
+++ palette-picker-vite/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     // GitHub Pages expects an index.html in the root directory
     // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
-    build: { outDir: 'finalapp' },
+    build: { outDir: '..' },
     // needed for github pages just put the repo name here
-    base: '/Patricks-palette-picker/',
-});
+    base: 'PatrickDacius/2-3-0-solo-project_palette-picker',
+}); 
